64115ed34a84c7b93453a1da4fdf81c68d2454aa
Lukyth <k.sujautra@gmail.com>
2014-09-22 14:34:22 +0700

    Add sound when turn

--- src/flappyx/MainGame.java
+++ src/flappyx/MainGame.java
@@ -1,15 +1,22 @@
 package flappyx;
 
+import java.io.IOException;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.openal.Audio;
+import org.newdawn.slick.openal.AudioLoader;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class MainGame extends BasicGame {
 
 	private Player player;
+	private Audio wavEffect;
+	
 	public static final int GAME_WIDTH = 640;
 	public static final int GAME_HEIGHT = 480;
 	
@@ -22,9 +29,11 @@ public class MainGame extends BasicGame {
 		if (key == Input.KEY_SLASH) {
 			System.out.println("slash");
 			player.switchY();
+			wavEffect.playAsSoundEffect(1.0f, 1.0f, false);
 		}
 		if (key == Input.KEY_Z) {
 			player.switchX();
+			wavEffect.playAsSoundEffect(1.0f, 1.0f, false);
 		}
 	}
 	
@@ -36,6 +45,12 @@ public class MainGame extends BasicGame {
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		player = new Player(GAME_WIDTH/2, GAME_HEIGHT/2, 0f, 0.1f);
+	    try {
+			wavEffect = AudioLoader.getAudio("WAV", ResourceLoader.getResourceAsStream("res/bading.wav"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -48,7 +63,7 @@ public class MainGame extends BasicGame {
 			MainGame game = new MainGame("Flappy X"); 
 			AppGameContainer container = new AppGameContainer(game);
 			container.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
-			container.setMinimumLogicUpdateInterval(1000 / 60);
+			container.setMinimumLogicUpdateInterval(1000/20);
 			container.start();
 		} catch (SlickException e) {
 			e.printStackTrace();
